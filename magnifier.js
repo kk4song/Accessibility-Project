8915bf6e7fbf39c8cbee2c7d22294f7d926aad18
kellysong1 <149431845+kk4song@users.noreply.github.com>
2024-11-21 12:17:26 -0500

    commit

--- magnifier.js
+++ magnifier.js
@@ -1,33 +1,51 @@
-function magnify(img, zoom) {
-    const glass = document.createElement("div");
+function magnify(imgID, zoom) {
+    var img, glass, w, h, bw;
+    img = document.getElementById(imgID);
+    glass = document.createElement("div");
     glass.setAttribute("class", "img-magnifier-glass");
-    document.body.appendChild(glass);
+    img.parentElement.insertBefore(glass, img);
 
-    glass.style.backgroundImage = `url('${img.src}')`;
+    glass.style.backgroundImage = "url('" + img.src + "')";
     glass.style.backgroundRepeat = "no-repeat";
-    glass.style.backgroundSize = `${img.width * zoom}px ${img.height * zoom}px`;
+    glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
 
-    const bw = 3;
-    const w = glass.offsetWidth / 2;
-    const h = glass.offsetHeight / 2;
+    bw = 3;
+    w = glass.offsetWidth / 2;
+    h = glass.offsetHeight / 2;
 
-    img.addEventListener("mousemove", moveMagnifier);
     glass.addEventListener("mousemove", moveMagnifier);
+    img.addEventListener("mousemove", moveMagnifier);
+
+    glass.addEventListener("touchmove", moveMagnifier);
+    img.addEventListener("touchmove", moveMagnifier);
 
     function moveMagnifier(e) {
-        const rect = img.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
+        var pos, x, y;
 
-        if (x > img.width || y > img.height || x < 0 || y < 0) {
-            glass.style.display = "none";
-            return;
-        }
+        e.preventDefault();
+        pos = getCursorPos(e);
+        x = pos.x;
+        y = pos.y;
 
-        glass.style.display = "block";
-        glass.style.left = `${e.pageX - w}px`;
-        glass.style.top = `${e.pageY - h}px`;
+        if (x > img.width - (w / zoom)) { x = img.width - (w / zoom); }
+        if (x < w / zoom) { x = w / zoom; }
+        if (y > img.height - (h / zoom)) { y = img.height - (h / zoom); }
+        if (y < h / zoom) { y = h / zoom; }
 
-        glass.style.backgroundPosition = `-${(x * zoom - w)}px -${(y * zoom - h)}px`;
+        glass.style.left = (x - w) + "px";
+        glass.style.top = (y - h) + "px";
+        glass.style.backgroundPosition = "-" + ((x * zoom) - w) + "px -" + ((y * zoom) - h) + "px";
+    }
+
+    function getCursorPos(e) {
+        var a, x = 0, y = 0;
+        e = e || window.event;
+        a = img.getBoundingClientRect();
+
+        x = e.pageX - a.left - window.scrollX;
+        y = e.pageY - a.top - window.scrollY;
+        return { x: x, y: y };
     }
 }
+
+magnify("myimage", 3);
